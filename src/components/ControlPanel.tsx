49276d5d699b892d815a87c3eadbb30fd5a09d04
Juan David Salamanca <salamancajd14@gmail.com>
2025-04-05 14:55:57 +0200

    Fix theming issues

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -3,7 +3,7 @@ import { TypographyH1 } from "./typography/H1";
 
 const ControlPanel = () => {
   return (
-    <div className="h-screen max-h-screen w-md bg-slate-700 p-4">
+    <div className="h-screen max-h-screen w-md bg-slate-100 dark:bg-slate-600 p-4">
       <div className="flex justify-between items-center">
         <TypographyH1> Micro Trace</TypographyH1>
         <ModeToggle />
