21ca718ddd3fec1e18568bf5e9b995b35813576e
Juan David Salamanca <salamancajd14@gmail.com>
2025-04-05 17:09:07 +0200

    Add input port

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,13 +1,25 @@
 import { ModeToggle } from "./menu-toggle";
 import { TypographyH1 } from "./typography/H1";
+import { TypographyP } from "./typography/P";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Separator } from "./ui/separator";
 
 const ControlPanel = () => {
   return (
-    <div className="h-screen max-h-screen w-md bg-slate-100 dark:bg-slate-600 p-4">
+    <div className="h-screen max-h-screen w-md bg-slate-100 dark:bg-slate-600 p-4 flex flex-col items-stretch gap-4">
       <div className="flex justify-between items-center">
         <TypographyH1> Micro Trace</TypographyH1>
         <ModeToggle />
       </div>
+      <div>
+        <TypographyP>Add a new port</TypographyP>
+
+        <Input />
+      </div>
+      <Button>+</Button>
+
+      <Separator orientation="horizontal" />
     </div>
   );
 };
